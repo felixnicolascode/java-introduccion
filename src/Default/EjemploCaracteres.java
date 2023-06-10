d98253c70372c94b9cb58e124b38261f0f9fee47
Felix <felixnicolas-m@hotmail.com>
2023-06-10 15:03:12 -0300

    EjemploCaracteres - Char - String - Ejemplos

---
+++ src/Default/EjemploCaracteres.java
@@ -0,0 +1,26 @@
+package Default;
+
+public class EjemploCaracteres {
+    public static void main(String[] args) {
+        // char
+        char caracter = 'a';
+        System.out.println(caracter);
+
+        char caracter2 = 65;
+        System.out.println(caracter2);
+
+        char caracter3 = 65 + 1;
+        System.out.println(caracter3);
+
+        char segundoCaracter = (char) (caracter3 + 1);
+        System.out.println(segundoCaracter);
+
+        // String
+        String palabra = "Alura cursos online";
+        System.out.println(palabra);
+
+        String palabra2 = palabra + "2020";
+        System.out.println(palabra2);
+
+    }
+}
