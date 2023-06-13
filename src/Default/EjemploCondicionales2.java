e91aac3f609be1e74762efe1da87950878d6894f
Felix <felixnicolas-m@hotmail.com>
2023-06-13 17:52:30 -0300

    EjemploCondicionales2 - Estructura condicional IF-ELSE - Ejemplo de optimización de código

---
+++ src/Default/EjemploCondicionales2.java
@@ -0,0 +1,17 @@
+package Default;
+
+public class EjemploCondicionales2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int edad = 17;
+        int cantidadPersonas = 2; // hacer variables autoexplicativas
+
+        if (edad >= 18 && cantidadPersonas >= 2){
+            System.out.println("Usted puede entrar.");
+
+        }else {
+            System.out.println("Usted no puede entrar.");
+        }
+
+    }
+}
