3fdadbfc1a9cf6d59e583dd0f79a4ef13df95ff5
Felix <felixnicolas-m@hotmail.com>
2023-06-13 18:08:35 -0300

    EjemploScoope - Estructura condicional IF-ELSE - Ejemplo de optimización de código

--- src/Default/EjemploCondicionales2.java
+++ src/Default/EjemploCondicionales2.java
@@ -4,7 +4,7 @@ public class EjemploCondicionales2 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int edad = 17;
-        int cantidadPersonas = 2; // hacer variables autoexplicativas
+        int cantidadPersonas = 2;
 
         if (edad >= 18 && cantidadPersonas >= 2){
             System.out.println("Usted puede entrar.");
