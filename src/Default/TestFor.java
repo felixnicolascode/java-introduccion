ba03e77042f54102537b31e249bfefcd8d962b7a
Felix <felixnicolas-m@hotmail.com>
2023-06-13 21:40:29 -0300

    TestFor - Ciclo For - Ejemplo

--- src/Default/TestFor.java
+++ src/Default/TestFor.java
@@ -4,7 +4,11 @@ public class TestFor {
 
     public static void main(String[] args) {
         for(int contador = 0; contador <= 10; contador++) {
-            System.out.println(contador);
+            for (int multiplicacion = 0; multiplicacion <= 10; multiplicacion++){
+                System.out.print(contador*multiplicacion);
+                System.out.print(" ");
+            }
+            System.out.println();
         }
     }
 
