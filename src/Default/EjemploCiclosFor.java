5428a5815b377d6a1ba1fcc1b512ca7a6839124a
Felix <felixnicolas-m@hotmail.com>
2023-06-13 21:18:27 -0300

    EjemploCiclosFor - Ciclo For - Ejemplos

---
+++ src/Default/EjemploCiclosFor.java
@@ -0,0 +1,10 @@
+package Default;
+
+public class EjemploCiclosFor {
+    public static void main(String[] args) {
+        //       variable      condicion     ejecutar AL FINAL
+        for (int contador = 0; contador <= 10; contador++) {
+            System.out.println(contador);
+        }
+    }
+}
